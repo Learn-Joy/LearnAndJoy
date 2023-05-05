99e599afb7e3ec63d4d2269725f9f70b5d44fda0
Berk Rende <rendeberk@gmail.com>
2023-05-05 14:22:03 +0300

    Home page with 3 menu items(Home, Add Questions, Account)

--- app/src/main/java/com/mirzahansuslu/learnandjoy/MainActivity.kt
+++ app/src/main/java/com/mirzahansuslu/learnandjoy/MainActivity.kt
@@ -3,10 +3,42 @@ package com.mirzahansuslu.learnandjoy
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.mirzahansuslu.learnandjoy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomePageFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(HomePageFragment())
+                R.id.add_questions -> replaceFragment(AddQuestionsFragment())
+                R.id.account -> replaceFragment(AccountFragment())
+
+                else -> {
+                    //do nothing
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+
     }
 }
